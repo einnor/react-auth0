a8892fe2c84c89ca6bcc4b875508426d16a442d1
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-07 13:50:38 +0300

    Toggle betweeen View Profile and Login actions

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
-  render() {
-    return <h1>Home</h1>;
-  }
+    render() {
+        const { isAuthenticated, login } = this.props.auth;
+        return (
+            <div>
+                <h1>Home</h1>
+                {
+                    isAuthenticated() ? (
+                        <Link to="/profile">View profile</Link>
+                    ) : (
+                        <button onClick={login}>Log In</button>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default Home;
