28aa973f61ebeecd35953943b04205ede4943ab6
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-07 15:54:34 +0300

    Register route for Public component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from "./Profile";
 import Nav from "./Nav";
 import Auth from "./Auth/Auth";
 import Callback from "./Callback";
+import Public from "./Public";
 
 class App extends Component {
   constructor(props) {
@@ -20,6 +21,7 @@ class App extends Component {
           <Route path="/" exact render={props => <Home auth={this.auth} {...props} />} />
           <Route path="/callback" render={props => <Callback auth={this.auth} {...props} />} />
           <Route path="/profile" render={props => this.auth.isAuthenticated() ? <Profile auth={this.auth} {...props} /> : <Redirect to="/" />} />
+          <Route path="/public" component={Public} />
         </div>
       </>
     );
