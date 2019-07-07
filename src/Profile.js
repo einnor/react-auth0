e3440162008f581874a2f7f3d9546baef3349f3f
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-07 15:28:23 +0300

    Render profile info

--- src/Profile.js
+++ src/Profile.js
@@ -17,7 +17,20 @@ class Profile extends Component {
     }
 
     render() {
-        return <h1>Profile</h1>;
+        const { profile } = this.state;
+        if (!profile) return null;
+        return (
+            <>
+                <h1>Profile</h1>
+                <p>{profile.nickname}</p>
+                <img
+                    style={{ maxWidth: 50, maxHeight: 50 }}
+                    src={profile.picture}
+                    alt="profile pic"
+                />
+                <pre>{JSON.stringify(profile, null, 2)}</pre>
+            </>
+        );
     }
 }
 
