a1eb369b948f538fbd3137174506e2c165e1f323
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-07 15:39:40 +0300

    Public route

--- server.js
+++ server.js
@@ -25,5 +25,11 @@ const checkJwt = jwt({
 
 const app = express();
 
+app.get("/public", function(req, res) {
+    res.json({
+      message: "Hello from a public API!"
+    });
+});
+
 app.listen(3001);
 console.log("API server listening on " + process.env.REACT_APP_AUTH0_AUDIENCE);
